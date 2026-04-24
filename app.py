3d2fd6ca56eaa937404a30fbabc077e65f309c98
tej600 <tschintawar2004@gmail.com>
2026-04-24 18:26:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, File, UploadFile
+import numpy as np
+import cv2
+from fastapi.responses import Response
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "API is running"}
+
+@app.post("/process")
+async def process_image(file: UploadFile = File(...)):
+    contents = await file.read()
+
+    npimg = np.frombuffer(contents, np.uint8)
+    img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Your logic
+    img[gray > 120] = [0, 255, 0]
+
+    _, buffer = cv2.imencode(".jpg", img)
+
+    return Response(content=buffer.tobytes(), media_type="image/jpeg")
+if __name__=="__main__":
+    app.run()
+app=app
